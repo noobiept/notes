0adafdcb1670b2ae029055ede1b976bfb37fa0f9
Pedro Ferreira <plferreira4@gmail.com>
2016-02-13 00:05:46 +0000

    - add gruntjs build script - remove previous build script

---
+++ build/Gruntfile.js
@@ -0,0 +1,84 @@
+module.exports = function( grunt )
+{
+var root = '../';
+var dest = '../release/<%= pkg.name %> <%= pkg.version %>/';
+
+grunt.initConfig({
+        pkg: grunt.file.readJSON( 'package.json' ),
+
+            // remove the destination folder first
+        clean: {
+            options: {
+                force: true
+            },
+
+            release: [
+                dest
+            ]
+        },
+
+            // copy the necessary files
+        copy: {
+            release: {
+                expand: true,
+                cwd: root,
+                src: [
+                    'images/icon16.png',
+                    'images/icon128.png',
+                    'libraries/**',
+                    'background.js',
+                    'manifest.json',
+                    'options.html'
+                ],
+                dest: dest
+            }
+        },
+
+            // minimize the javascript
+        uglify: {
+            release: {
+                files: [{
+                    src: [
+                        root + 'js/*.js'
+                    ],
+                    dest: dest + 'min.js'
+                }]
+            }
+        },
+
+            // minimize the css
+        cssmin: {
+            release: {
+                files: [{
+                    expand: true,
+                    cwd: root + 'css/',
+                    src: '*.css',
+                    dest: dest + 'css/'
+                }]
+            },
+            options: {
+                advanced: false
+            }
+        },
+
+            // update the html file to load the min.js file
+        processhtml: {
+            release: {
+                expand: true,
+                cwd: root,
+                src: 'index.html',
+                dest: dest
+            }
+        }
+    });
+
+    // load the plugins
+grunt.loadNpmTasks( 'grunt-contrib-clean' );
+grunt.loadNpmTasks( 'grunt-contrib-copy' );
+grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
+grunt.loadNpmTasks( 'grunt-processhtml' );
+
+    // tasks
+grunt.registerTask( 'default', [ 'clean', 'copy', 'uglify', 'cssmin', 'processhtml' ] );
+};
