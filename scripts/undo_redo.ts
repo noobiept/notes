a9cc1db0c36b928989570ddbf4d2b331b683c07d
Pedro Ferreira <plferreira4@gmail.com>
2017-04-20 22:46:51 +0100

    - convert the 'UndoRedo' module to typescript - add 'jquery' and 'jqueryui' types to the dev dependencies - fix the include path in the typescript configuration

--- scripts/undo_redo.ts
+++ scripts/undo_redo.ts
@@ -1,5 +1,3 @@
-/*global Menu, MAIN_CONTAINER*/
-
 /*
  * Works for:
  *
@@ -7,10 +5,10 @@
  *      - removed notes
  *      - dragged notes
  */
-var UndoRedo = {
-    undo_list : [],
-    redo_list : []
-};
+module UndoRedo
+{
+var UNDO_LIST = [];
+var REDO_LIST = [];
 
 
 /*
@@ -20,82 +18,82 @@ var UndoRedo = {
  *      noteObject (Note) : the object of the note in question
  *      usefulStuff  (??) : depends on 'what' is -- see in the code
  */
-UndoRedo.add = function( what, noteObject, usefulStuff )
-{
-switch( what )
+export function add( what, noteObject, usefulStuff )
     {
-    case 'addedNote':
+    switch( what )
+        {
+        case 'addedNote':
 
-        UndoRedo.saveAddedNote( what, noteObject );
-        break;
+            saveAddedNote( what, noteObject );
+            break;
 
-    case 'removedNote':
+        case 'removedNote':
 
-        UndoRedo.saveRemovedNote( what, noteObject );
-        break;
+            saveRemovedNote( what, noteObject );
+            break;
 
-    case 'draggedNote':
+        case 'draggedNote':
 
-        UndoRedo.saveDraggedNote( what, noteObject, usefulStuff );
-        break;
+            saveDraggedNote( what, noteObject, usefulStuff );
+            break;
+        }
     }
-};
 
 
-UndoRedo.saveAddedNote = function( what, noteObject )
-{
-UndoRedo.undo_list.push({
+function saveAddedNote( what, noteObject )
+    {
+    UNDO_LIST.push({
 
-    what        : what,
-    position    : noteObject.getPosition(),
-    text        : "",
-    colorObject : noteObject.getColorObject()
+        what        : what,
+        position    : noteObject.getPosition(),
+        text        : "",
+        colorObject : noteObject.getColorObject()
 
-    });
+        });
 
-    //on a new change, clean the REDO, so that it doesn't create conflicts
-UndoRedo.cleanRedo();
-};
-
-
-UndoRedo.saveRemovedNote = function( what, noteObject )
-{
-UndoRedo.undo_list.push({
-
-    what        : what,
-    position    : noteObject.getPosition(),
-    text        : noteObject.getText(),
-    colorObject : noteObject.getColorObject()
-
-    });
-
-    //on a new change, clean the REDO, so that it doesn't create conflicts
-UndoRedo.cleanRedo();
-};
+        //on a new change, clean the REDO, so that it doesn't create conflicts
+    cleanRedo();
+    }
 
 
-UndoRedo.saveDraggedNote = function( what, noteObject, previousPosition )
-{
-UndoRedo.undo_list.push({
+function saveRemovedNote( what, noteObject )
+    {
+    UNDO_LIST.push({
 
-    what             : what,
-    position         : noteObject.getPosition(),
-    previousPosition : previousPosition
+        what        : what,
+        position    : noteObject.getPosition(),
+        text        : noteObject.getText(),
+        colorObject : noteObject.getColorObject()
 
-    });
+        });
 
-    //on a new change, clean the REDO, so that it doesn't create conflicts
-UndoRedo.cleanRedo();
-};
+        //on a new change, clean the REDO, so that it doesn't create conflicts
+    cleanRedo();
+    }
+
+
+function saveDraggedNote( what, noteObject, previousPosition )
+    {
+    UNDO_LIST.push({
+
+        what             : what,
+        position         : noteObject.getPosition(),
+        previousPosition : previousPosition
+
+        });
+
+        //on a new change, clean the REDO, so that it doesn't create conflicts
+    cleanRedo();
+    }
 
 
 /*
  * When there's a new change in the list, we clean the redo_obj to avoid conflicts with the undo later on
  */
-UndoRedo.cleanRedo = function ()
-{
-UndoRedo.redo_list.length = 0;
-};
+function cleanRedo()
+    {
+    REDO_LIST.length = 0;
+    }
 
 
 /*
@@ -108,12 +106,12 @@ UndoRedo.redo_list.length = 0;
  *
  * So, before we undo/redo, we need to update the text/background-color
  */
-UndoRedo.updateNote = function(element, noteObject)
-{
-    //update the text (since it could have been changed)
-element.text = noteObject.getText();
-element.colorObject = noteObject.getColorObject();
-};
+function updateNote(element, noteObject)
+    {
+        //update the text (since it could have been changed)
+    element.text = noteObject.getText();
+    element.colorObject = noteObject.getColorObject();
+    }
 
 
 /*
@@ -121,127 +119,128 @@ element.colorObject = noteObject.getColorObject();
  *
  *      whichOne (string) : "undo" or "redo"
  */
-UndoRedo.stuff = function( whichOne )
-{
-var element;
-
-if (whichOne === 'undo')
+export function stuff( whichOne )
     {
-    if (UndoRedo.undo_list.length === 0)
+    var element;
+
+    if (whichOne === 'undo')
         {
-        Menu.showMessage('Nothing to restore');
-        return;
+        if (UNDO_LIST.length === 0)
+            {
+            Menu.showMessage('Nothing to restore');
+            return;
+            }
+
+            //get last element
+        element = UNDO_LIST.pop();
+
+            //send it to the redo array
+        REDO_LIST.push( element );
         }
 
-        //get last element
-    element = UndoRedo.undo_list.pop();
-
-        //send it to the redo array
-    UndoRedo.redo_list.push( element );
-    }
-
-else        //redo
-    {
-    if (UndoRedo.redo_list.length === 0)
+    else        //redo
         {
-        Menu.showMessage('Nothing to redo');
-        return;
+        if (REDO_LIST.length === 0)
+            {
+            Menu.showMessage('Nothing to redo');
+            return;
+            }
+
+            //get last element
+        element = REDO_LIST.pop();
+
+            //send it to the UNDO array
+        UNDO_LIST.push( element );
         }
 
-        //get last element
-    element = UndoRedo.redo_list.pop();
+    var temp;
 
-        //send it to the UNDO array
-    UndoRedo.undo_list.push( element );
-    }
+    if (element.what === 'removedNote')
+        {
+        if (whichOne === 'undo')
+            {
+            var colorComponents = {
 
-var temp;
+                    red          : element.colorObject.red_int,
+                    green        : element.colorObject.green_int,
+                    blue         : element.colorObject.blue_int,
+                    alpha        : element.colorObject.alpha_float,
+                    wasSetByUser : element.colorObject.wasSetByUser_bool
+                };
 
-if (element.what === 'removedNote')
+            MAIN_CONTAINER.newNote( element.text, colorComponents, false, element.position );
+            }
+
+            //redo - we remove the previously added entry (by the undo)
+        else
+            {
+            temp = MAIN_CONTAINER.getChild( element.position );
+
+                //update the data on the element (some stuff could have been changed since it was first saved to UndoRedo)
+            updateNote( element, temp );
+
+            temp.remove( false );
+            }
+        }
+
+
+    else if (element.what === 'addedNote')
+        {
+        if (whichOne === 'undo') // same as 'removedNote' and 'redo'
+            {
+            temp = MAIN_CONTAINER.getChild( element.position );
+
+                //update the data on the element (some stuff could have been changed since it was first saved to UndoRedo)
+            updateNote( element, temp );
+
+            temp.remove( false );
+            }
+
+        else    // same as 'removedNote' and 'undo'
+            {
+            var colorComponents2 = {
+
+                    red          : element.colorObject.red_int,
+                    green        : element.colorObject.green_int,
+                    blue         : element.colorObject.blue_int,
+                    alpha        : element.colorObject.alpha_float,
+                    wasSetByUser : element.colorObject.wasSetByUser_bool
+                };
+
+            MAIN_CONTAINER.newNote( element.text, colorComponents2, false, element.position );
+            }
+        }
+
+    else if (element.what === 'draggedNote')
     {
     if (whichOne === 'undo')
         {
-        var colorComponents = {
-
-                red          : element.colorObject.red_int,
-                green        : element.colorObject.green_int,
-                blue         : element.colorObject.blue_int,
-                alpha        : element.colorObject.alpha_float,
-                wasSetByUser : element.colorObject.wasSetByUser_bool
-            };
-
-        MAIN_CONTAINER.newNote( element.text, colorComponents, false, element.position );
+            //move without saving it again to undo
+        MAIN_CONTAINER.getChild( element.position ).moveTo( element.previousPosition, false );
         }
 
-        //redo - we remove the previously added entry (by the undo)
     else
         {
-        temp = MAIN_CONTAINER.getChild( element.position );
-
-            //update the data on the element (some stuff could have been changed since it was first saved to UndoRedo)
-        UndoRedo.updateNote( element, temp );
-
-
-        temp.remove( false );
+            //move without saving it again to undo
+        MAIN_CONTAINER.getChild( element.previousPosition ).moveTo( element.position, false );
         }
     }
 
+        // what is in the form: likeThis
+        // separate the words with a space
+    var what = element.what.replace(/[A-Z]/, function(match) { return " " + match; });
 
-else if (element.what === 'addedNote')
-    {
-    if (whichOne === 'undo') // same as 'removedNote' and 'redo'
+        // capitalize everything (first letter upper case)
+    var capitalize = function(theString)
         {
-        temp = MAIN_CONTAINER.getChild( element.position );
+        return theString.replace(/\b[a-z]/i, function(match) { return match.toUpperCase(); });
+        };
 
-            //update the data on the element (some stuff could have been changed since it was first saved to UndoRedo)
-        UndoRedo.updateNote( element, temp );
+    what = capitalize( what );
+    whichOne = capitalize( whichOne );
 
-
-        temp.remove( false );
-        }
-
-    else    // same as 'removedNote' and 'undo'
-        {
-        var colorComponents2 = {
-
-                red          : element.colorObject.red_int,
-                green        : element.colorObject.green_int,
-                blue         : element.colorObject.blue_int,
-                alpha        : element.colorObject.alpha_float,
-                wasSetByUser : element.colorObject.wasSetByUser_bool
-            };
-
-        MAIN_CONTAINER.newNote( element.text, colorComponents2, false, element.position );
-        }
+    Menu.showMessage( whichOne + ": " + what );
     }
+}
 
-else if (element.what === 'draggedNote')
-   {
-   if (whichOne === 'undo')
-       {
-          //move without saving it again to undo
-       MAIN_CONTAINER.getChild( element.position ).moveTo( element.previousPosition, false );
-       }
 
-   else
-       {
-          //move without saving it again to undo
-       MAIN_CONTAINER.getChild( element.previousPosition ).moveTo( element.position, false );
-       }
-   }
-
-    // what is in the form: likeThis
-    // separate the words with a space
-var what = element.what.replace(/[A-Z]/, function(match) { return " " + match; });
-
-    // capitalize everything (first letter upper case)
-var capitalize = function(theString)
-    {
-    return theString.replace(/\b[a-z]/i, function(match) { return match.toUpperCase(); });
-    };
-
-what = capitalize( what );
-whichOne = capitalize( whichOne );
-
-Menu.showMessage( whichOne + ": " + what );
-};
