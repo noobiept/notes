32e775425c2b1571c65bb6d6bcf0c98f6c583799
Pedro Ferreira <plferreira4@gmail.com>
2017-04-21 00:29:32 +0100

    - convert the 'Color' and 'MainContainer' classes to typescript

--- scripts/color.ts
+++ scripts/color.ts
@@ -1,111 +1,120 @@
+class Color
+{
+wasSetByUser_bool: boolean;
+red_int: number;
+green_int: number;
+blue_int: number;
+alpha_float: number;
+
+
 /**
  * Represents a color.
  */
-function Color(red, green, blue, alpha, wasSetByUser)
-{
-if (typeof wasSetByUser === 'undefined')
+constructor( red, green, blue, alpha, wasSetByUser )
     {
-    this.wasSetByUser_bool = false;
+    if (typeof wasSetByUser === 'undefined')
+        {
+        this.wasSetByUser_bool = false;
+        }
+
+    else
+        {
+        this.wasSetByUser_bool = wasSetByUser;
+        }
+
+    this.red_int = red;
+    this.green_int = green;
+    this.blue_int = blue;
+    this.alpha_float = alpha;
     }
 
-else
+
+getCssRepresentation()
     {
-    this.wasSetByUser_bool = wasSetByUser;
+    return "rgba(" + this.red_int + ", " + this.green_int + ", " + this.blue_int + ", " + this.alpha_float  + ")";
     }
 
-this.red_int = red;
-this.green_int = green;
-this.blue_int = blue;
-this.alpha_float = alpha;
-}
+
+getRed()
+    {
+    return this.red_int;
+    }
 
 
-Color.prototype.getCssRepresentation = function()
-{
-return "rgba(" + this.red_int + ", " + this.green_int + ", " + this.blue_int + ", " + this.alpha_float  + ")";
-};
+setRed( red )
+    {
+    this.red_int = red;
+
+    this.wasSetByUser_bool = true;
+    }
 
 
-Color.prototype.getRed = function()
-{
-return this.red_int;
-};
+getGreen()
+    {
+    return this.green_int;
+    }
 
 
-Color.prototype.setRed = function( red )
-{
-this.red_int = red;
+setGreen( green )
+    {
+    this.green_int = green;
 
-this.wasSetByUser_bool = true;
-};
-
-
-Color.prototype.getGreen = function()
-{
-return this.green_int;
-};
+    this.wasSetByUser_bool = true;
+    }
 
 
-Color.prototype.setGreen = function( green )
-{
-this.green_int = green;
-
-this.wasSetByUser_bool = true;
-};
+getBlue()
+    {
+    return this.blue_int;
+    }
 
 
-Color.prototype.getBlue = function()
-{
-return this.blue_int;
-};
+setBlue( blue )
+    {
+    this.blue_int = blue;
+
+    this.wasSetByUser_bool = true;
+    }
 
 
-Color.prototype.setBlue = function( blue )
-{
-this.blue_int = blue;
-
-this.wasSetByUser_bool = true;
-};
+getAlpha()
+    {
+    return this.alpha_float;
+    }
 
 
-Color.prototype.getAlpha = function()
-{
-return this.alpha_float;
-};
+setAlpha( alpha )
+    {
+    this.alpha_float = alpha;
 
-
-Color.prototype.setAlpha = function( alpha )
-{
-this.alpha_float = alpha;
-
-this.wasSetByUser_bool = true;
-};
+    this.wasSetByUser_bool = true;
+    }
 
 
 /*
  * Returns a dictionary that has as keys, the components of the final color, red/green/blue/alpha
  *        (red/green/blue varies from 0 to 255, alpha from 0 to 1)
  */
-Color.prototype.getColor = function()
-{
-return {
-    red   : this.red_int,
-    green : this.green_int,
-    blue  : this.blue_int,
-    alpha : this.alpha_float,
-    wasSetByUser : this.wasSetByUser_bool
-    };
-};
+getColor()
+    {
+    return {
+        red   : this.red_int,
+        green : this.green_int,
+        blue  : this.blue_int,
+        alpha : this.alpha_float,
+        wasSetByUser : this.wasSetByUser_bool
+        };
+    }
 
 
 /*
  * The colors that aren't set by the user aren't saved, they're generated again when the program starts (and change depending on the style used to generate them)
  *
  */
-Color.prototype.wasSetByUser = function()
-{
-return this.wasSetByUser_bool;
-};
+wasSetByUser()
+    {
+    return this.wasSetByUser_bool;
+    }
 
 
 /*
@@ -115,7 +124,8 @@ return this.wasSetByUser_bool;
  *
  *      yesNo (bool)
  */
-Color.prototype.canBeGenerated = function( yesNo )
-{
-this.wasSetByUser_bool = !yesNo;
-};
+canBeGenerated( yesNo )
+    {
+    this.wasSetByUser_bool = !yesNo;
+    }
+}
--- scripts/main_container.ts
+++ scripts/main_container.ts
@@ -1,17 +1,20 @@
-/*global Note, DummyNote*/
+class MainContainer
+{
+htmlElement_obj: HTMLElement;
+childrenObjects_array: Note[];
+dummyNote_obj: DummyNote;
+
 
 /*
  * container of all the notes (there can be only one)
  *
  */
-function MainContainer()
-{
-this.htmlElement_obj = document.querySelector( '#mainContainer' );
-this.childrenObjects_array = [];
-this.dummyNote_obj = null;
-
-return this;
-}
+constructor()
+    {
+    this.htmlElement_obj = <HTMLElement> document.querySelector( '#mainContainer' );
+    this.childrenObjects_array = [];
+    this.dummyNote_obj = null;
+    }
 
 
 /*
@@ -20,82 +23,82 @@ return this;
  *
  * Creates a note and adds it to the main container
  */
-MainContainer.prototype.newNote = function( text, noteComponents, saveToUndo, position, fromLoad )
-{
-var noteObject = new Note( this, text, noteComponents, saveToUndo, position, fromLoad );
-
-    // we're getting the position from the object and not from the argument, because the argument may not have an acceptable value
-var notePosition = noteObject.getPosition();
-
-var container = this.htmlElement_obj;
-
-    // add to the html container element
-container.insertBefore( noteObject.getHtmlElement(), container.childNodes[ notePosition ] );
-
-    //add to the array
-this.childrenObjects_array.splice( notePosition, 0, noteObject );
-
-    // only need to update when its not added at the end
-if ( this.childrenCount() !== notePosition + 1 )
+newNote( text, noteComponents, saveToUndo, position, fromLoad )
     {
-    this.updateOrder( notePosition );
+    var noteObject = new Note( this, text, noteComponents, saveToUndo, position, fromLoad );
+
+        // we're getting the position from the object and not from the argument, because the argument may not have an acceptable value
+    var notePosition = noteObject.getPosition();
+
+    var container = this.htmlElement_obj;
+
+        // add to the html container element
+    container.insertBefore( noteObject.getHtmlElement(), container.childNodes[ notePosition ] );
+
+        //add to the array
+    this.childrenObjects_array.splice( notePosition, 0, noteObject );
+
+        // only need to update when its not added at the end
+    if ( this.childrenCount() !== notePosition + 1 )
+        {
+        this.updateOrder( notePosition );
+        }
+
+    return noteObject;
     }
 
-return noteObject;
-};
+
+addDummyNote()
+    {
+    this.dummyNote_obj = new DummyNote();
+
+    this.htmlElement_obj.appendChild( this.dummyNote_obj.getHtmlElement() );
+
+        // has to be called after it is appended, otherwise the calculations will give 'wrong' results
+    this.dummyNote_obj.centerText();
+    }
 
 
-MainContainer.prototype.addDummyNote = function()
-{
-this.dummyNote_obj = new DummyNote();
-
-this.htmlElement_obj.appendChild( this.dummyNote_obj.getHtmlElement() );
-
-    // has to be called after it is appended, otherwise the calculations will give 'wrong' results
-this.dummyNote_obj.centerText();
-};
+getDummy()
+    {
+    return this.dummyNote_obj;
+    }
 
 
-MainContainer.prototype.getDummy = function()
-{
-return this.dummyNote_obj;
-};
-
-
-MainContainer.prototype.getHtmlElement = function()
-{
-return this.htmlElement_obj;
-};
+getHtmlElement = function()
+    {
+    return this.htmlElement_obj;
+    }
 
 
 /*
  * returns the first child element object (null if it doesn't exist)
  */
-MainContainer.prototype.getFirstChild = function()
-{
-if (this.childrenObjects_array.length !== 0)
+getFirstChild()
     {
-    return this.childrenObjects_array[0];
-    }
+    if (this.childrenObjects_array.length !== 0)
+        {
+        return this.childrenObjects_array[0];
+        }
 
-return null;
-};
+    return null;
+    }
 
 
 /*
  * returns the last child object (null if it doesn't exist)
  */
-MainContainer.prototype.getLastChild = function()
-{
-var children = this.childrenObjects_array;
-
-if (children.length !== 0)
+getLastChild()
     {
-    return children[ children.length - 1 ];
-    }
+    var children = this.childrenObjects_array;
 
-return null;
-};
+    if (children.length !== 0)
+        {
+        return children[ children.length - 1 ];
+        }
+
+    return null;
+    }
 
 
 /*
@@ -104,25 +107,25 @@ return null;
  * Arguments:
  *      - position (number)
  */
-MainContainer.prototype.getChild = function( position )
-{
-    //see if it exists
-if ( position >= this.childrenCount() || position < 0)
+getChild( position )
     {
-    return null;
-    }
+        //see if it exists
+    if ( position >= this.childrenCount() || position < 0)
+        {
+        return null;
+        }
 
-return this.childrenObjects_array[ position ];
-};
+    return this.childrenObjects_array[ position ];
+    }
 
 
 /*
  * returns the number of children elements (do note, that it may not make sense (for example for an entry.. it doesn't have children))
  */
-MainContainer.prototype.childrenCount = function()
-{
-return this.childrenObjects_array.length;
-};
+childrenCount()
+    {
+    return this.childrenObjects_array.length;
+    }
 
 
 /*
@@ -130,48 +133,49 @@ return this.childrenObjects_array.length;
  *
  *      lessPosition (int) : from what position we start updating the order, as in, where did the change occur (the elements before have the right order)
  */
-MainContainer.prototype.updateOrder = function( lessPosition )
-{
-if (typeof lessPosition === 'undefined')
+updateOrder( lessPosition )
     {
-    lessPosition = 0;
-    }
-
-var notes = this.getHtmlElement().childNodes;
-
-var i;
-
-for (i = lessPosition ; i < notes.length ; i++)
-    {
-        //apply only to notes
-    if ( notes[i].classList.contains( 'dummyNote' ) === false )
+    if (typeof lessPosition === 'undefined')
         {
-        notes[i].noteObject.position_int = i;
+        lessPosition = 0;
         }
-    }
 
-    //sort array
-this.childrenObjects_array.sort(
-    function(a, b)
+    var notes = this.getHtmlElement().childNodes;
+
+    var i;
+
+    for (i = lessPosition ; i < notes.length ; i++)
         {
-        return a.position_int - b.position_int;
+            //apply only to notes
+        if ( notes[i].classList.contains( 'dummyNote' ) === false )
+            {
+            notes[i].noteObject.position_int = i;
+            }
         }
-    );
-};
+
+        //sort array
+    this.childrenObjects_array.sort(
+        function(a, b)
+            {
+            return a.position_int - b.position_int;
+            }
+        );
+    }
 
 
 /**
  * Returns a list with all the notes text.
  */
-MainContainer.prototype.getTextList = function()
-{
-var children = this.childrenObjects_array;
-var textList = [];
-
-for (var a = 0 ; a < children.length ; a++)
+getTextList()
     {
-    textList.push( children[ a ].getText() );
-    }
+    var children = this.childrenObjects_array;
+    var textList = [];
 
-return textList;
-};
+    for (var a = 0 ; a < children.length ; a++)
+        {
+        textList.push( children[ a ].getText() );
+        }
+
+    return textList;
+    }
+}
